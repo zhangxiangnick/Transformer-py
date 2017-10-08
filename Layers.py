6a90770e3b6f2d7314d5a28f49a228274a960ee9
Xiang Zhang <xzhang23@ncsu.edu>
2017-10-08 15:24:22 -0700

    Add basic wrappers of one encoder layer and one decoder layer

--- Layers.py
+++ Layers.py
@@ -36,7 +36,7 @@ class MultiHeadAttention(nn.Module):
         query: batch_size x len_query x d_model 
         key:   batch_size x len_key x d_model   
         value: batch_size x len_key x d_model
-        mask:  batch_size x len_key
+        mask:  batch_size x len_query x len_key or broadcastable 
         
     Outputs Shapes:
         out:   batch_size x len_query x d_model
@@ -80,7 +80,7 @@ class MultiHeadAttention(nn.Module):
         attns = torch.bmm(proj_query, proj_key.transpose(1,2))  # batch_size*h x len_query x len_key
         attns = attns / math.sqrt(self.d_head) 
         attns = attns.view(b, self.h, len_query, len_key) 
-        attns = attns.masked_fill_(mask.unsqueeze(1).unsqueeze(1), -float('inf'))
+        attns = attns.masked_fill_(mask.unsqueeze(1), -float('inf'))
         attns = self.sm(attns.view(-1, len_key)).view(b*self.h, len_query, len_key)
         
         # apply attns on value
@@ -90,6 +90,7 @@ class MultiHeadAttention(nn.Module):
         out = self.layernorm(query + self.dropout(out))   
         return out
 
+    
 class FeedForward(nn.Module):
     """
     Applies position-wise feed forward to inputs
@@ -123,4 +124,79 @@ class FeedForward(nn.Module):
     def forward(self, input):
         out = self.dropout(self.fc_2(self.relu(self.fc_1(input))))
         out = self.layernorm(out + input)
+        return out
+
+    
+class EncoderLayer(nn.Module):
+    """
+    Wraps multi-head attentions and position-wise feed forward into one encoder layer
+    
+    Args:
+        h:       number of heads
+        d_model: dimention of model
+        p:       dropout probabolity 
+        d_ff:    dimension of feed forward
+        
+    Params:
+        multihead:    multi-head attentions layer
+        feedforward:  feed forward layer
+    
+    Input Shapes:
+        query: batch_size x len_query x d_model 
+        key:   batch_size x len_key x d_model   
+        value: batch_size x len_key x d_model
+        mask:  batch_size x len_query x len_key or broadcastable 
+    
+    Output Shapes:
+        out: batch_size x len_query x d_model
+    """
+    
+    def __init__(self, h, d_model, p, d_ff):
+        super(EncoderLayer, self).__init__()
+        self.multihead = MultiHeadAttention(h, d_model, p)
+        self.feedforward = FeedForward(d_model, d_ff, p)
+    
+    def forward(self, query, key, value, mask):
+        out = self.multihead(query, key, value, mask)
+        out = self.feedforward(out)
+        return out
+    
+    
+class DecoderLayer(nn.Module):
+    """
+    Wraps multi-head attentions and position-wise feed forward into one layer of decoder
+    
+    Args:
+        h:       number of heads
+        d_model: dimention of model
+        p:       dropout probabolity 
+        d_ff:    dimension of feed forward
+        
+    Params:
+        multihead_tgt:  multi-head self attentions layer
+        multihead_src:  multi-head encoder-decoder attentions layer        
+        feedforward:    feed forward layer
+    
+    Input Shapes:
+        query:    batch_size x len_query x d_model 
+        key:      batch_size x len_key x d_model   
+        value:    batch_size x len_key x d_model
+        context:  batch_size x len_src x d_model
+        mask_tgt: batch_size x len_query x len_key or broadcastable 
+        mask_src: batch_size x len_query x len_src or broadcastable 
+    
+    Output Shapes:
+        out: batch_size x len_query x d_model
+    """    
+    
+    def __init__(self, h, d_model, p, d_ff):
+        super(DecoderLayer, self).__init__()
+        self.multihead_tgt = MultiHeadAttention(h, d_model, p)
+        self.multihead_src = MultiHeadAttention(h, d_model, p)
+        self.feedforward = FeedForward(d_model, d_ff, p)
+    
+    def forward(self, query, key, value, context, mask_tgt, mask_src):
+        out = self.multihead_tgt(query, key, value, mask_tgt)
+        out = self.multihead_src(out, context, context, mask_src)
+        out = self.feedforward(out)
         return out
