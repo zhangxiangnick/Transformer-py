a5b54a031b874963fb65a4fdd5271e4dd7fe59f2
Xiang Zhang <xzhang23@ncsu.edu>
2017-10-11 23:15:18 -0700

    Update scripts for preparing data

--- download_en_de.sh
+++ download_en_de.sh
@@ -13,21 +13,21 @@ sudo make install
 sudo ldconfig -v
 cd ..
 
-# download wmt16 data
-mkdir data
-cd data
+# download data
+mkdir translation-data
+cd translation-data
 wget http://www.statmt.org/europarl/v7/de-en.tgz
-wget http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz
-wget http://data.statmt.org/wmt16/translation-task/training-parallel-nc-v11.tgz
+wget http://data.statmt.org/wmt17/translation-task/rapid2016.tgz
+wget http://data.statmt.org/wmt17/translation-task/training-parallel-nc-v12.tgz
 wget http://data.statmt.org/wmt16/translation-task/dev.tgz
 wget http://data.statmt.org/wmt16/translation-task/test.tgz
 tar -xvzf de-en.tgz
-tar -xvzf training-parallel-commoncrawl.tgz
-tar -xvzf training-parallel-nc-v11.tgz
+tar -xvzf rapid2016.tgz
+tar -xvzf training-parallel-nc-v12.tgz
 tar -xvzf dev.tgz
 tar -xvzf test.tgz
-cat europarl-v7.de-en.en commoncrawl.de-en.en training-parallel-nc-v11/news-commentary-v11.de-en.en > train.en
-cat europarl-v7.de-en.de commoncrawl.de-en.de training-parallel-nc-v11/news-commentary-v11.de-en.de > train.de
+cat europarl-v7.de-en.en rapid2016.de-en.en training/news-commentary-v12.de-en.en > train.en
+cat europarl-v7.de-en.de rapid2016.de-en.de training/news-commentary-v12.de-en.de > train.de
 git clone https://github.com/moses-smt/mosesdecoder.git
 mosesdecoder/scripts/ems/support/input-from-sgm.perl < dev/newstest2014-deen-src.de.sgm > dev/newstest2014.de
 mosesdecoder/scripts/ems/support/input-from-sgm.perl < dev/newstest2014-deen-ref.en.sgm > dev/newstest2014.en
@@ -37,16 +37,15 @@ mosesdecoder/scripts/ems/support/input-from-sgm.perl < test/newstest2016-deen-sr
 mosesdecoder/scripts/ems/support/input-from-sgm.perl < test/newstest2016-deen-ref.en.sgm > test/newstest2016.en
 cat dev/newstest2014.en dev/newstest2015.en > dev.en
 cat dev/newstest2014.de dev/newstest2015.de > dev.de
-cd ..
 
 # bpe training and encoding
-spm_train --input=data/train.en,data/train.de --model_prefix=en_de_bp3_37000 --vocab_size=37000 --model_type=bpe
-spm_encode --model=en_de_bp3_37000.model --output_format=id < data/train.en > data/train.en.id
-spm_encode --model=en_de_bp3_37000.model --output_format=id < data/train.de > data/train.de.id
-spm_encode --model=en_de_bp3_37000.model --output_format=id < data/dev.en > data/dev.en.id
-spm_encode --model=en_de_bp3_37000.model --output_format=id < data/dev.de > data/dev.de.id
-spm_encode --model=en_de_bp3_37000.model --output_format=id < data/test/newstest2016.en > data/test/newstest2016.en.id
-spm_encode --model=en_de_bp3_37000.model --output_format=id < data/test/newstest2016.de > data/test/newstest2016.de.id
+spm_train --input=train.en,train.de --model_prefix=de-en_bp3_32000 --vocab_size=32000 --model_type=bpe
+spm_encode --model=de-en_bp3_32000.model --output_format=id < train.de > train.de.id
+spm_encode --model=de-en_bp3_32000.model --output_format=id --extra_options=bos:eos < train.en > train.en.id
+spm_encode --model=de-en_bp3_32000.model --output_format=id < dev.de > dev.de.id
+spm_encode --model=de-en_bp3_32000.model --output_format=id --extra_options=bos:eos < dev.en > dev.en.id
+spm_encode --model=de-en_bp3_32000.model --output_format=id < test/newstest2016.de > test/newstest2016.de.id
+spm_encode --model=de-en_bp3_32000.model --output_format=id --extra_options=bos:eos < test/newstest2016.en > test/newstest2016.en.id
 
 
 
